cafa9e59535bc52645613f0bede290df884e9f0f
Atila Braga <braga.atila@gmail.com>
2015-05-07 01:24:26 -0300

    Activity lifecycle example to explore.

--- app/src/main/java/com/atilabraga/cursoandroid/MainActivity.java
+++ app/src/main/java/com/atilabraga/cursoandroid/MainActivity.java
@@ -2,11 +2,11 @@ package com.atilabraga.cursoandroid;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import android.util.Log;
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final String CLASS_TAG = MainActivity.class.getCanonicalName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,24 +15,32 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onStart() {
+        super.onStart();
+        Log.i(CLASS_TAG, "onStart");
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onResume() {
+        super.onResume();
+        Log.i(CLASS_TAG, "onResume");
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(CLASS_TAG, "onPause");
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(CLASS_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(CLASS_TAG, "onDestroy");
     }
 }
