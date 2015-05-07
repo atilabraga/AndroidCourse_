42b81538dfd2ad5f6425c1139afd12d3c5e5bf48
Atila Braga <braga.atila@gmail.com>
2015-05-07 01:44:59 -0300

    Opening another activity.

---
+++ app/src/main/java/com/atilabraga/cursoandroid/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.atilabraga.cursoandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Átila on 07/05/2015.
+ */
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
